539c001367b42c502ab41b71d67478b1a801029e
SolBellomo <sbellomo@udesa.edu.ar>
2020-05-28 22:52:45 -0300

    paso tortuga

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function myFunction() {
-  var x = document.getElementById("myTopnav");
+  let x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
@@ -7,3 +7,25 @@ function myFunction() {
   }
 }
 
+//CHART
+
+let proxy = 'https://cors-anywhere.herokuapp.com/'
+let endpoint = 'https://api.deezer.com/chart/0'
+
+fetch (proxy+endpoint) 
+.then(function(dataJson){
+  return dataJson.json()
+})
+.then(function(resultado){
+  let radio = resultado.playlists;
+  let charts = document.querySelector('.audiios');
+
+  for(let i=0; i<8; i++){  
+    charts.img.innerHTML += '<img src="' + data[i].picture + '">'
+    charts.innerHTML += '<audio class="audiios" src="' + charts.tracks.link[i].preview + '" controls>' + '</audio>'
+    return console.log(resultado);
+  }
+})
+.catch(function(error){
+  return console.log(error);
+})
