3a4781f08474b52458035ad14b38030515f2c3d1
jelizalde21 <jelizalde@udesa.edu.ar>
2020-06-09 17:05:00 -0300

    pushhh

--- generoDetail.js
+++ generoDetail.js
@@ -8,11 +8,11 @@ fetch(url)
     .then(function(datos){
         console.log(datos);
     
-        let artists = document.querySelector('artistass');
-        let resultados = datos.data;
+        let artists = document.querySelector('.artistass');
+        let resultados = datos.title;
 
         resultados.forEach(function(artist){
-            artistass.innerHTML += '<li>' + '<a href="artistsDetail.html?id=' + artist.id + '">' + artist.name  + '</a></li>s'
+            artistass.innerHTML += datos.genre.name
         })
     })
     .catch(function(error){
@@ -27,7 +27,7 @@ fetch(url)
     .then(function(datos){
     console.log(datos);
 
-    let artists = document.querySelector('topp5');
+    let artists = document.querySelector('.topp5');
     let resultados = datos.data;
 
     resultados.forEach(function(artist){
@@ -45,14 +45,42 @@ fetch(url)
     .then(function(datos){
         console.log(datos);
     
-        let artists = document.querySelector('favoritoss');
+        let artists = document.querySelector('.favoritoss');
         let resultados = datos.data;
 
         resultados.forEach(function(artist){
-            artistass.innerHTML += '<li>' + '<a href="playlists.html?id=' + artist.id + '">' + artist.name  + '</a></li>s'
+            artistass.innerHTML += '<li>' + '<a href="artistsDetail.html?id=' + artist.id + '">' + artist.name  + '</a></li>'
         })
     })
     .catch(function(error){
         console.log(error)
     })
 
+    let proxy = 'https://cors-anywhere.herokuapp.com/'
+let url = proxy + 'https://api.deezer.com/chart/0/artists'
+
+fetch (url) 
+  .then(function(dataJson){
+  return dataJson.json()
+  })
+  .then(function(resultado){
+    console.log(resultado);
+    let artistaArray = resultado.data; 
+    let charts = document.querySelector('.artistass');
+    let divColumna = '';
+
+  for(let i=0; i<artistaArray.length; i++){ 
+
+    divColumna += '<div class="columna">';
+    divColumna += '      <div class="img-container">'; 
+    divColumna += '        <img src="' +  artistaArray[i].picture + '" alt="' + artistaArray[i].name + '" class= "img">';
+    divColumna += '      </div>';
+    divColumna += '        <a href="artistsDetail.html?id=' + artistaArray[i].id + '"' + ' class="artista1">' + artistaArray[i].name + '</a>';
+    divColumna += '    </div>';
+  }
+  charts.innerHTML = divColumna;  
+})
+
+.catch(function(error){
+  return console.log(error);
+})
