c3d0580c27b72541a64aa10decc9fc947c18c7b0
SolBellomo <sbellomo@udesa.edu.ar>
2020-06-14 20:33:24 -0300

    algunas aclaracionesss

--- carruselIndex.js
+++ carruselIndex.js
@@ -7,23 +7,18 @@ showDivs(slideIndex);
 function fotosExtra(fotos) {
   showDivs(slideIndex += fotos);
 }
-function showDivs(start){
+function showDivs(start){ // % --> resto de la división que quiero mostrar --> a%b te devuelve un numero entre 0 y b-1
   let carrusel = document.getElementsByClassName("seccionArtistass");
-  console.log(carrusel);
-  start = start % carrusel.length;
-  console.log(start);
-  let totalToShow = 5;
-  if(carrusel.length < totalToShow){
-      start = 0;
-      totalToShow = carrusel.length;
+  start = start % carrusel.length; // devuelve numero entre 0 y carrusel.length - 1 (en este caso entre 0 y 9)
+  let totalQueQuieroMostrar = 5; 
+  if(carrusel.length < totalQueQuieroMostrar){ //Si hay menos de 5 elementos que los muestre todos
+      totalQueQuieroMostrar = carrusel.length;
   } else {
-    for(i = start + totalToShow; i%carrusel.length != start%carrusel.length; i++){
-      console.log('Bye ' + i);
-      carrusel[i%carrusel.length].style.display = 'none';
+    for(i = start + totalQueQuieroMostrar; i%carrusel.length != start%carrusel.length; i++){ 
+      carrusel[i%carrusel.length].style.display = 'none'; //Para que no me muestre si traigo más de 5
     }
   }
-  for(i = start; i< start + totalToShow; i++){
-      console.log('Hi ' + i);
+  for(i = start; i< start + totalQueQuieroMostrar; i++){
       carrusel[i% carrusel.length].style.display = 'block';
   }
 }
