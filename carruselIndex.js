b4ff0b78a3f7cefa6a4da4eb6a87b041598a8050
SolBellomo <sbellomo@udesa.edu.ar>
2020-06-15 01:38:32 -0300

    arreglemo

--- carruselIndex.js
+++ carruselIndex.js
@@ -22,3 +22,74 @@ function showDivs(start){ // % --> resto de la división que quiero mostrar --> 
       carrusel[i% carrusel.length].style.display = 'block';
   }
 }
+
+//CARRUSEL ALBUMS
+let slideIndex1 = 5;
+showDivs1(slideIndex1);
+
+function fotossExtra(fotoss) {
+  showDivs1(slideIndex1 += fotoss);
+}
+function showDivs1(startt){ 
+  let carrusel2 = document.getElementsByClassName("seccionAlbumss");
+  startt = startt % carrusel2.length; 
+  let totalQueQuieroMostrarr = 5; 
+  if(carrusel2.length < totalQueQuieroMostrarr){ 
+      totalQueQuieroMostrarr = carrusel2.length;
+  } else {
+    for(i = startt + totalQueQuieroMostrarr; i%carrusel2.length != startt%carrusel2.length; i++){ 
+      carrusel2[i%carrusel2.length].style.display = 'none'; 
+    }
+  }
+  for(i = startt; i< startt + totalQueQuieroMostrarr; i++){
+      carrusel2[i% carrusel2.length].style.display = 'block';
+  }
+}
+
+//CARRUSEL GENRES
+
+let slideIndexGenres = 5;
+showDivsGenres(slideIndexGenres);
+
+function fotosExtra4(fotosGenres) {
+  showDivsGenres(slideIndexGenres += fotosGenres);
+}
+function showDivsGenres(start4){ 
+  let carruselGenres = document.getElementsByClassName("seccionGenres");
+  start4 = start4 % carruselGenres.length; 
+  let GenerosQueQuieroMostrar = 5; 
+  if(carruselGenres.length < GenerosQueQuieroMostrar){ 
+    GenerosQueQuieroMostrar = carruselGenres.length;
+  } else {
+    for(i = start4 + GenerosQueQuieroMostrar; i%carruselGenres.length != start4%carruselGenres.length; i++){ 
+      carruselGenres[i%carruselGenres.length].style.display = 'none'; 
+    }
+  }
+  for(i = start4; i< start4 + GenerosQueQuieroMostrar; i++){
+    carruselGenres[i% carruselGenres.length].style.display = 'block';
+  }
+}
+
+//CARRUSEL TRACKS
+
+let slideIndexTracks = 5;
+showDivsTracks(slideIndexTracks);
+
+function fotosExtra3(fotosTracks) {
+  showDivsTracks(slideIndexTracks += fotosTracks);
+}
+function showDivsTracks(start3){ 
+  let carruselTracks = document.getElementsByClassName("seccionTracks");
+  start3 = start3 % carruselTracks.length; 
+  let TracksQueQuieroMostrar = 5; 
+  if(carruselTracks.length < TracksQueQuieroMostrar){ 
+    TracksQueQuieroMostrar = carruselTracks.length;
+  } else {
+    for(i = start3 + TracksQueQuieroMostrar; i%carruselTracks.length != start3%carruselTracks.length; i++){ 
+      carruselTracks[i%carruselTracks.length].style.display = 'none'; 
+    }
+  }
+  for(i = start3; i< start3 + TracksQueQuieroMostrar; i++){
+    carruselTracks[i% carruselTracks.length].style.display = 'block';
+  }
+}
