a93ed5a0575627cbc188fa15922a781f49d2606a
jelizalde21 <jelizalde@udesa.edu.ar>
2020-06-16 16:48:58 -0300

    genero artist

---
+++ generoDetail.js
@@ -0,0 +1,59 @@
+
+
+let proxy = 'https://cors-anywhere.herokuapp.com/'
+let urlGenero = proxy + 'https://api.deezer.com/genre/' + id
+/*
+fetch(urlGenero)
+  .then(function(response){
+    return response.json();
+  })
+  .then(function(datos){
+    console.log(datos);
+    let image = document.querySelector(".imagen-genero");
+    image.innerHTML = '<img src="' + datos.picture_medium + '" alt="">';
+  })
+  .catch(function(error){
+    console.log(error);
+  })
+
+fetch()
+.then(function(response){
+  return response.json();
+})
+.then(function(datos){
+  let image = document.querySelector(".cancionesss");
+  image.innerHTML = '<img src="' + datos.picture_medium + '" alt="img">';
+
+  let titulos = document.querySelector('.cancionesss');
+  titulos.innerHTML = '<a href=""></a>' 
+})*/
+
+//A rezar
+
+fetch(urlGenero)
+    .then.then(function(response){
+        return response.json();
+        })
+        .then(function(datos){
+        console.log(datos);
+        let image = document.querySelector(".artistasss");
+        image.innerHTML += '<img src="' + datos.picture_big + '>';
+
+        let titulo = document.querySelector('.titulo-artist');
+        titulo.innerHTML += datos.name;
+        let urlGenre = urlGenero + 'artists'
+
+        fetch(urlGenre)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(datos){
+            console.log(datos)
+        })
+        .catch(function(error){
+            console.log(error)
+        })
+    })
+.catch(function(error){
+    console.log(error)
+})
