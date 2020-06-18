6bf1b1e423fcf13da780e8397c42bfd61d084da4
jelizalde21 <jelizalde@udesa.edu.ar>
2020-06-18 12:45:03 -0300

    Retoque en TracksDetail

--- trackDetail.js
+++ trackDetail.js
@@ -12,11 +12,11 @@ fetch(url)
 })
 .then(function(datos){
     console.log(datos);
-    let titulo = document.querySelector('.titulo');
+    let titulo = document.querySelector('.tituloTrack');
     titulo.innerHTML += datos.title;
 
     let cancion = document.querySelector('.song');
-    cancion.innerHTML += '<iframe scrolling="no" frameborder="0" allowTransparency="true" src="https://www.deezer.com/plugins/player?format=square&autoplay=false&playlist=false&width=300&height=300&color=007FEB&layout=dark&size=medium&type=tracks&id=' + idTrack + '&app_id=1" width="300" height="300"></iframe>'
+    cancion.innerHTML += '<iframe scrolling="no" frameborder="0" allowTransparency="true" src="https://www.deezer.com/plugins/player?format=square&autoplay=false&playlist=false&width=300&height=300&color=007FEB&layout=dark&size=medium&type=tracks&id=' + idTrack + '&app_id=1" width="220" height="220"></iframe>'
 
 
     let interprete = document.querySelector('.interprete');
