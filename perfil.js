b4d670cfe536abe878038b9ae06eecd96e326684
Pablo-pujol <ppujol@udesa.edu.ar>
2020-06-18 22:41:17 -0300

    Iniciar sesion

--- perfil.js
+++ perfil.js
@@ -1,31 +1,22 @@
+
 let queryString = location.search;
 let datos = new URLSearchParams(queryString);
 let email1 = datos.get('email')
 let pw1 = datos.get('password')
 
 localStorage.setItem('userName', email1);
-localStorage.setItem('userPw', pw1)
+localStorage.setItem('userPw', pw1);
 
-var recuperoMail = localStorage.getItem('userName');
-var recuperoPw = localStorage.getItem('userPw');
+let recuperoMail = localStorage.getItem('userName');
+let recuperoPw = localStorage.getItem('userPw');
 
-var iniciar = document.getElementById('boton') 
-var contra = document.getElementsByClassName('formsContra')
-var elmail = document.getElementsByClassName('formsMail')
-
-console.log(elmail);
-
+let iniciar = document.querySelector('#boton'); 
 
 iniciar.onclick = function(){
-
-    if (elmail.ariaAutoComplete == null || contra.ariaAutoComplete == null){    
-    alert('Completa los datos.');
-} else {
-    let email = document.getElementById('email');
-    email.innerHTML = recuperoMail;
+    let inicio = document.querySelector('.inicio');
+    inicio.innerHTML = '<img src="imagenes/.png" alt="Inicio">';
 }
 
-}
 
 
 
